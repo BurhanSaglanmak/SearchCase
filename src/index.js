ca49b51d6271476c04c932ff61a188116889bdd9
Burhan Sağlanmak <104764065+BurhanSaglanmak@users.noreply.github.com>
2024-07-02 21:53:38 +0300

    root.render update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 /* eslint-disable */
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./assets/scss/main.css";
 import App from "./App";
 import { SearchProvider } from "./contexts/useSearchContext";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <SearchProvider>
       <App />
     </SearchProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
